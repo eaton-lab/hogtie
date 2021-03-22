cb31f5319e7795719117392e5cea924f31444ed6
Rachel Cohen <cohen.rachel@columbia.edu>
2021-03-22 09:44:05 -0400

    updated main to parse csv

--- hogtie/__main__.py
+++ hogtie/__main__.py
@@ -5,9 +5,9 @@ Command line interface for hogtie
 """
 
 import argparse
-import os
+import sys
+#import pandas as pd #eventually change this to convert matrix to df when I can parse matrix
 import toytree
-import pandas as pd
 from hogtie import Pagel
 
 
@@ -22,13 +22,14 @@ def parse_command_line():
     parser.add_argument('--data',
         nargs='?',
         type=argparse.FileType('r'),
-        help='Input is binary character trait data in a csv file'
+        help='Input is binary character trait data in a csv file',
+        default=sys.stdin
         )
 
     parser.add_argument('--tree',
         nargs='?',
         type=argparse.FileType('r'),
-        help='tree in newick format with edge lengths and support vaues'
+        help='tree in newick format with edge lengths and support values'
         )
 
     args = parser.parse_args()
@@ -38,19 +39,20 @@ def main():
     """
     Runs Pagel on parsed args
     """
+    print('Reading in data and tree...')
     args = parse_command_line()
-    print('Reading in data...')
     mydata = args.data.read()
-    mydata = pd.DataFrame(mydata)
-    print('Data read in as a pandas dataframe')
-
-    #mydata = list(mydata.split(','))
-
-    #mytree = args.tree.read()
-    #mytree = toytree.tree(mytree, tree_format=0)
-
-    #liketree = Pagel(tree=mytree, data=mydata)
-    #liketree.run()
+    mydata = mydata.split(',')
+    mydata = [int(i) for i in mydata] 
+ 
+    mytree = args.tree.read()
+    mytree = toytree.tree(mytree, tree_format=0)
+ 
+    print('Getting conditional likelihoods...')
+    liketree = Pagel(tree=mytree, data=mydata)
+    liketree.run()
+    print('The conditional likelihoods are:')
+    print(liketree.tree.get_node_values('likelihood',True,True))
 
 
 #if __name__ == "__main__":
