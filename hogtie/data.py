fe20fcc8cf74ee5b14e0f907cc320991c80cb4a8
Deren <de2356@columbia.edu>
2021-03-23 13:05:46 -0400

    sim data example

--- hogtie/data.py
+++ hogtie/data.py
@@ -7,12 +7,20 @@ generating data using ipcoal
 import ipcoal
 import toytree
 
+
 # if this is meant to be executed then you should put it in main
 # otherwise it would be run if this file was imported. 
 if __name__ == "__main__":
-	tree = toytree.rtree.unittree(ntips=10, treeheight=1e5)
-	mod = ipcoal.Model(tree=tree, Ne=1e6, admixture_edges=[(3, 8, 0.5, 0.5)], nsamples=1)
-	mod.sim_loci(1, nsites=10000)
-	genos = mod.write_vcf()
-	genos.iloc[:, 9:].T
 
+    # sample a 10 tip imbalanced species tree
+    tree = toytree.rtree.imbtree(ntips=10, treeheight=1e5)
+
+    # setup a demographic model with high ILS
+    mod = ipcoal.Model(tree=tree, Ne=1e5, nsamples=1)
+
+    # simulate 1 long chromosome
+    mod.sim_loci(nloci=1, nsites=100000)
+
+    # write haploid genotypes to VCF file
+    genos = mod.write_vcf(outdir="../examples", name="highILS")
+    # genos.iloc[:, 9:].T
