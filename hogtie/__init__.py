12fea92c87bc800c8c3eab3cfce64ec0b0d44740
Deren <de2356@columbia.edu>
2021-03-23 13:03:52 -0400

    funcs reorganized into a class, and added logger

---
+++ hogtie/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+Binary state character phylogenetic likelihood calculations
+"""
+
+from hogtie.utils import set_loglevel
+from hogtie.binary_state_model import BinaryStateModel
+
+__version__ = "0.0.2"
+
+set_loglevel("INFO")
--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 #! usr/bin/env python
 
+"""
+install with `pip install -e .`
+"""
+
 from setuptools import setup
 
 setup(
     name="hogtie",
-    version="0.0.1",
+    version="0.0.2",
     packages=[],
     entry_points={
-        }
+    }
 )
