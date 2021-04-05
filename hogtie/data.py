ee68cfc1621a941c7ee768697fbb520f2bfc52f1
Rachel Cohen <cohen.rachel@columbia.edu>
2021-04-05 10:25:35 -0400

    added simulation nb, some edits to data.py and matrixlike.py

--- hogtie/data.py
+++ hogtie/data.py
@@ -8,19 +8,17 @@ import ipcoal
 import toytree
 
 
-# if this is meant to be executed then you should put it in main
-# otherwise it would be run if this file was imported. 
+# simulating a tree with admixture edges between divergent tips close to the present 
 if __name__ == "__main__":
 
     # sample a 10 tip imbalanced species tree
     tree = toytree.rtree.imbtree(ntips=10, treeheight=1e5)
 
     # setup a demographic model with high ILS
-    mod = ipcoal.Model(tree=tree, Ne=1e5, nsamples=1)
+    mod = ipcoal.Model(tree=tree, Ne=1e5, nsamples=1, admixture_edges=(3,5,0.5,0.01))
 
     # simulate 1 long chromosome
-    mod.sim_loci(nloci=1, nsites=100000)
+    mod.sim_snps(1000)
 
     # write haploid genotypes to VCF file
     genos = mod.write_vcf(outdir="../examples", name="highILS")
-    # genos.iloc[:, 9:].T
--- hogtie/matrixlike.py
+++ hogtie/matrixlike.py
@@ -22,10 +22,23 @@ class MatrixParser:
     """
     def __init__(self, 
         tree,               #must be Toytree class object
-        file):       #must be .csv filetype - as filepath
+        matrix = None,      #must be pandas DataFrame class object
+        file = None,
+        model = None    
+        ):       #must be .csv filetype - as filepath
+
 
         self.tree = tree
-        self.matrix = pd.read_csv(file)
+        self.model = model 
+
+        if isinstance(matrix, pd.DataFrame):
+            self.matrix = matrix  
+
+        elif file != None:
+            self.matrix = pd.read_csv(file)
+
+        else:
+            raise ValueError("please supply matrix")
 
         #for i in self.matrix:
         #  if i != 1 or 0:
@@ -38,13 +51,15 @@ class MatrixParser:
         likelihoods = np.empty((0,len(self.matrix.columns)),float)
         for column in self.matrix:
             data = self.matrix[column]
-            out = BinaryStateModel(self.tree, data)
+            out = BinaryStateModel(self.tree, data, self.model)
             out.optimize()
         
             lik = out.log_lik
             likelihoods = np.append(likelihoods, lik)
          
             self.likelihoods = likelihoods
+
+    logger.debug(f'Calculated likelihoods for each column: {self.tree.get_node_values('likelihood',True,True)}')
 
     def threshold(self):
         """
@@ -58,6 +73,6 @@ if __name__ == "__main__":
     HOGTIEDIR = os.path.dirname(os.getcwd())
     tree1 = toytree.rtree.unittree(ntips=10)
     file1 = os.path.join(HOGTIEDIR, "sampledata", "testmatrix.csv")
-    testmatrix = MatrixParser(tree=tree1, file=file1)
+    testmatrix = MatrixParser(tree=tree1, file=file1, model='ARD')
     testmatrix.matrix_likelihoods()
     print(testmatrix.likelihoods)
